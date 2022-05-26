846733f08f39e444f3634c73c86794ebc0d4d202
vaibhav bhagat <84506606+vaibhavbhagat9545@users.noreply.github.com>
2022-05-27 00:00:44 +0530

    Add files via upload

---
+++ public/client.js
@@ -0,0 +1,75 @@
+const socket = io()
+
+let name;
+let textarea = document.querySelector('#textarea')
+let messagearea = document.querySelector('.message_area')
+
+do{
+    name = prompt('Enter user name :')
+}while(!name)
+    
+    
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter') {
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(message){
+    let msg = {
+        user: name,
+        message: message.trim()
+    }
+    
+    // Append message
+    
+    appendMessage(msg, 'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+    
+    // send on server
+    
+    socket.emit('message', msg)
+}
+
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className,'message')
+    
+    let markup = `
+          <h4>${msg.user}</h4>
+          <p>${msg.message}</p>
+                       `
+    
+    mainDiv.innerHTML = markup
+    messagearea.appendChild(mainDiv)
+}
+
+// Recive messages 
+
+socket.on('message',(msg) => {
+   appendMessage(msg, 'incoming')
+    scrollToBottom()
+})
+
+
+function scrollToBottom() {
+    messagearea.scrollTop = messagearea.scrollHeight
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
